6434db560d070bdb9b74466e1365d70d30a8ce5b
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-09-21 16:38:46 +0530

    hello

--- frontend/src/pages/home/NewDrop.jsx
+++ frontend/src/pages/home/NewDrop.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const NewDrop = () => {
+  const {items} = useSelector((state) => state.products)
+
+  useEffect(() =>{
+    const product = axios.fetch("")
+  } , [])
+  console.log(items);
+  
   return (
     <div className='min-h-screen'>
       <div className='w-full'>
      <h1 className='text-center font-bold py-12 text-2xl lg:text-4xl'>New Drop</h1>
 
+     <div>
+      {
+        items.map((product , idx) =>(
+          <img src={product.image} alt={product.name} />
+        ))
+      }
+     </div>
       </div>
     </div>
   )
