db312952f58fe970fac001158820e9b3cb5af84f
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-10-11 20:13:59 +0530

    Frontend fixed

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,23 +1,22 @@
-import React, { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, {Suspense, lazy} from "react";
+import {Routes, Route} from "react-router-dom";
 import AdminLayout from "../layout/AdminLayout";
 
 const Dashboard = lazy(() => import("../pages/admin/Dashboard"));
 const AddProducts = lazy(() => import("../pages/admin/AddProducts"));
 const Users = lazy(() => import("../pages/admin/Users"));
-const Orders = lazy(() => import("../pages/admin/Orders"));
 const Products = lazy(() => import("../pages/admin/Products"));
 const ErrorPage = lazy(() => import("../pages/ErrorPage"));
 
 const AdminRoutes = () => {
   return (
     <Routes>
-      <Route  element={<AdminLayout />}>
+      <Route element={<AdminLayout />}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="products" element={<Products />} />
         <Route path="add" element={<AddProducts />} />
         <Route path="users" element={<Users />} />
-        <Route path="orders" element={<Orders />} />
+
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
