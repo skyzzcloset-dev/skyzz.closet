45dea391da12747f489dea2d31aa7f7ca300753e
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-10-12 18:00:37 +0530

    checking product

--- frontend/src/features/products/productService.js
+++ frontend/src/features/products/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/product/";
+const API_URL = "https://skyzzcloset-product.up.railway.app/api/product/";
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("token");
