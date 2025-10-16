9fdec35ea0ce32c20c78bcbcd86de2e8aa4f944b
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-10-16 18:41:07 +0530

    redeploying order to checkkk it again

--- backend/order/src/app.js
+++ backend/order/src/app.js
@@ -12,8 +12,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://www.skyzzcloset.shop", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-    optionsSuccessStatus: 200,
   })
 );
 
