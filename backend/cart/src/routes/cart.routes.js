738822c63fc378f8af2c5a73a2fde187c003107e
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-10-26 23:06:09 +0530

    cart fixing

--- backend/cart/src/routes/cart.routes.js
+++ backend/cart/src/routes/cart.routes.js
@@ -1,5 +1,6 @@
+// backend/src/routes/cart.routes.js
 const express = require("express");
-const {createAuthMiddleware} = require("../middlewares/auth.middleware");
+const { createAuthMiddleware } = require("../middlewares/auth.middleware");
 const {
   addItemToCart,
   getCart,
@@ -9,20 +10,9 @@ const {
 
 const router = express.Router();
 
-// Only customers can add, update, get, or delete their own cart
-router.post("/items", createAuthMiddleware(), addItemToCart);
+router.post("/items", createAuthMiddleware(["customer"]), addItemToCart);
 router.get("/getItems", createAuthMiddleware(["customer"]), getCart);
-
-router.patch(
-  "/items/:productId",
-  createAuthMiddleware(["customer"]),
-  updateCart
-);
-
-router.delete(
-  "/items/:productId",
-  createAuthMiddleware(["customer"]),
-  deleteCart
-);
+router.patch("/items/:productId", createAuthMiddleware(["customer"]), updateCart);
+router.delete("/items/:productId", createAuthMiddleware(["customer"]), deleteCart);
 
 module.exports = router;
